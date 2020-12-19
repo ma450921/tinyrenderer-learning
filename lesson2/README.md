e81feab7696cf6222ad8299943f65307ef447ea0
ma450921 <450921734@qq.com>
2020-12-19 14:31:56 +0800

    triangle init

--- lesson2/README.md
+++ lesson2/README.md
@@ -33,10 +33,69 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
 ```
 
 ## fill rect
+
 得到三角形的轮廓之后，关键在于如何填充这个三角形。首先需要获取Y轴上填充的起点和终点, 通过替换三个点的变量值来保证 t0 t1 t2 三个点在y轴方向上的顺序
 ``` cpp
 if (t0.y > t1.y) std::swap(t0, t1);
 if (t0.y > t2.y) std::swap(t0, t2);
 if (t1.y > t2.y) std::swap(t1, t2);
 ```
+在此之后需要在y方向上进行遍历，开始从下往上逐行扫描，每次计算三角形与逐行扫描线的两个交点A和B，填充A与B中间的所有像素
+``` cpp
+int total_height = t2.y-t0.y; 
+for (int y=t0.y; y<=t1.y; y++) { 
+    int segment_height = t1.y-t0.y+1; 
+    float alpha = (float)(y-t0.y)/total_height; 
+    float beta  = (float)(y-t0.y)/segment_height; // be careful with divisions by zero 
+    Vec2i A = t0 + (t2-t0)*alpha; 
+    Vec2i B = t0 + (t1-t0)*beta; 
+    image.set(A.x, y, red); 
+    image.set(B.x, y, green); 
+}
+```
+以上代码步骤仅填充了三角形的下半部分，因为上半部分的角度需要通过t1重新计算如下：
+```cpp
+for (int y=t1.y; y<=t2.y; y++) { 
+    int segment_height =  t2.y-t1.y+1; 
+    float alpha = (float)(y-t0.y)/total_height; 
+    float beta  = (float)(y-t1.y)/segment_height; // be careful with divisions by zero 
+    Vec2i A = t0 + (t2-t0)*alpha; 
+    Vec2i B = t1 + (t2-t1)*beta; 
+    if (A.x>B.x) std::swap(A, B); 
+    for (int j=A.x; j<=B.x; j++) { 
+        image.set(j, y, color); // attention, due to int casts t0.y+i != A.y 
+    } 
+} 
+```
+将上述代码抽象为可读性更强的代码：
+``` cpp
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
+    if (t0.y==t1.y && t0.y==t2.y) return; // I dont care about degenerate triangles 
+    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+    if (t0.y>t1.y) std::swap(t0, t1); 
+    if (t0.y>t2.y) std::swap(t0, t2); 
+    if (t1.y>t2.y) std::swap(t1, t2); 
+    int total_height = t2.y-t0.y; 
+    for (int i=0; i<total_height; i++) { 
+        bool second_half = i>t1.y-t0.y || t1.y==t0.y; 
+        int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y; 
+        float alpha = (float)i/total_height; 
+        float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here 
+        Vec2i A =               t0 + (t2-t0)*alpha; 
+        Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta; 
+        if (A.x>B.x) std::swap(A, B); 
+        for (int j=A.x; j<=B.x; j++) { 
+            image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y 
+        } 
+    } 
+}
+```
 
+## 光栅化处理思路
+光栅化的处理思路和上述方法类似，归根到底还是遍历屏幕内的每一个像素判断是否在三角形内。
+判断是否在三角形内实际上是可以通过向量计算的方法来进行实现的。
+假定三角形的三个点分别为 p0 p1 p2
+判断的点为 q，计算三个点与q的连线，以及三个点顺序连线的叉乘，若三者同号则q在三角形内
+p0p1✖️p0q p1p2✖️p1q p2p0✖️p2q 
+
+另外，扫描整个屏幕的像素显然是不合理的，我们可以通过扫描三角形的包围盒从而来判断是否在三角形内
--- lesson2/main.cpp
+++ lesson2/main.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include <cmath>
+#include <iostream>
 #include "tgaimage.h"
 #include "geometry.h"
 
@@ -36,34 +36,61 @@ const int height = 200;
 //     line(t2, t0, image, green);
 // }
 
-void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
-    if (t0.y > t1.y) std::swap(t0, t1);
-    if (t0.y > t2.y) std::swap(t0, t2);
-    if (t1.y > t2.y) std::swap(t1, t2);
-    int total_height = t2.y - t0.y;
-    for (int y = t0.y; y < t1.y; y++) {
-        int segment_height = t1.y - t0.y + 1;
-        float alpha = (float)(y - t0.y) / total_height;
-        float beta = (float)(y - t0.y) / segment_height;
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = t0 + (t1 - t0) * beta;
-        if (A.x > B.x) std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++) {
-            image.set(j, y, color);
-        }
-    }
-    for (int y = t1.y; y < t2.y; y++) {
-        int segment_height = t2.y - t1.y + 1;
-        float alpha = (float)(y - t0.y) / total_height;
-        float beta = (float)(y - t1.y) / segment_height;
-        Vec2i A = t0 + (t2 - t0) * alpha;
-        Vec2i B = t1 + (t2 - t1) * beta;
-        if (A.x > B.x) std::swap(A, B);
-        for (int j = A.x; j <= B.x; j++) {
-            image.set(j, y, color);
-        }
-    }
-}
+// 逐行扫描确定三角形范围绘制三角形
+// void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) { 
+//     if (t0.y==t1.y && t0.y==t2.y) return; // I dont care about degenerate triangles 
+//     // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+//     if (t0.y>t1.y) std::swap(t0, t1); 
+//     if (t0.y>t2.y) std::swap(t0, t2); 
+//     if (t1.y>t2.y) std::swap(t1, t2); 
+//     int total_height = t2.y-t0.y; 
+//     for (int i=0; i<total_height; i++) { 
+//         bool second_half = i>t1.y-t0.y || t1.y==t0.y; 
+//         int segment_height = second_half ? t2.y-t1.y : t1.y-t0.y; 
+//         float alpha = (float)i/total_height; 
+//         float beta  = (float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here 
+//         Vec2i A =               t0 + (t2-t0)*alpha; 
+//         Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta; 
+//         if (A.x>B.x) std::swap(A, B); 
+//         for (int j=A.x; j<=B.x; j++) { 
+//             image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y 
+//         } 
+//     } 
+// }
+
+/**
+ * 向量方式进行计算
+ * 通过判断点是否在三角形内来进行绘制
+ */
+ 
+Vec3f barycentric(Vec2i *pts, Vec2i P) { 
+    Vec3f u = cross(Vec3f(pts[2][0]-pts[0][0], pts[1][0]-pts[0][0], pts[0][0]-P[0]), Vec3f(pts[2][1]-pts[0][1], pts[1][1]-pts[0][1], pts[0][1]-P[1]));
+    /* `pts` and `P` has integer value as coordinates
+       so `abs(u[2])` < 1 means `u[2]` is 0, that means
+       triangle is degenerate, in this case return something with negative coordinates */
+    if (std::abs(u[2])<1) return Vec3f(-1,1,1);
+    return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
+} 
+ 
+void triangle(Vec2i *pts, TGAImage &image, TGAColor color) { 
+    Vec2i bboxmin(image.get_width()-1,  image.get_height()-1); 
+    Vec2i bboxmax(0, 0); 
+    Vec2i clamp(image.get_width()-1, image.get_height()-1); 
+    for (int i=0; i<3; i++) { 
+        for (int j=0; j<2; j++) { 
+            bboxmin[j] = std::max(0,        std::min(bboxmin[j], pts[i][j])); 
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j])); 
+        } 
+    } 
+    Vec2i P; 
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) { 
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) { 
+            Vec3f bc_screen  = barycentric(pts, P); 
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue; 
+            image.set(P.x, P.y, color); 
+        } 
+    } 
+} 
 int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
 
